ce37604e2bca24b83ca93b62df898191d48dc862
leslieportiz <lorraineportiz@gmail.com>
2023-03-25 22:14:42 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-
+import StoreListing from './components/StoreListing';
+import TopBar from './components/TopBar';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <TopBar/>
+      <StoreListing/>
     </div>
   );
 }
---
+++ src/components/TopBar.js
@@ -0,0 +1,19 @@
+import {Navbar,Nav} from 'react-bootstrap'
+// import '../css/navbar.css'
+export default function TopBar() {
+    return <Navbar bg="light" expand="lg" >
+    <Navbar.Brand style={{margin: "5px 0 0 20px"}} href="#home">
+        <img src="[Instacart logo src]" /> 
+    </Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto"> 
+        </Nav>
+        <Nav>
+            <Nav.Link href="#LogIn">Log In</Nav.Link>
+            <Button variant="success">Sign Up</Button>{' '}
+        </Nav>
+    </Navbar.Collapse>
+</Navbar>
+}
+
